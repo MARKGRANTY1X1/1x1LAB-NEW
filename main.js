146b4a2a589c20c423f31dad9c537f3eece130bb
mark granty <115198916+MARKGRANTY1X1@users.noreply.github.com>
2025-10-18 17:05:02 -0400

    add main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  win.loadURL('https://copy-of-copy-of-crypto-mining-2-1046645786545.us-west1.run.app')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
